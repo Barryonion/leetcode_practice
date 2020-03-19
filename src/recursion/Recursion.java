4b61cd867c314836aea1feee51959e4f8517613f
10156618 <zhangxucheng-hq@boe.com.cn>
2020-03-19 17:31:01 +0800

    1.新增递归练习之斐波那契数列

---
+++ src/recursion/Recursion.java
@@ -0,0 +1,39 @@
+package recursion;
+
+public class Recursion {
+    private int[] results;
+
+    public Recursion() {}
+    public Recursion(int n) {
+        this.results = new int[n];
+    }
+    private int fibonacciFunctionV1(int n) {/*基础版*/
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return fibonacciFunctionV1(n - 1) + fibonacciFunctionV1(n - 2);
+    }
+
+    private int fibonacciFunctionV2(int n) {/*进化版*/
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        /*新增散列表存储计算结果*/
+        if (results[n - 1] != 0) {
+            return results[n-1];
+        }
+        results[n-1] = fibonacciFunctionV2(n - 1) + fibonacciFunctionV2(n - 2);
+        return results[n-1];
+    }
+
+    public static void main(String[] args) {
+        int n = 50;
+//        Recursion recursion = new Recursion();
+        Recursion recursion = new Recursion(n);
+        long startTime = System.nanoTime();
+//        int result = recursion.fibonacciFunctionV1(n);
+        int result = recursion.fibonacciFunctionV2(n);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("斐波那契数列第" + n + "项值为： " + result);
+        System.out.println("fibonacciFunctionV1函数总用时:" + totalTime + "ns");
+    }
+}
