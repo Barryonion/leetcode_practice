b1e648929a3c8927a329a77eb649331ae7a97bd9
zhang <zhangxucheng991@gmail.com>
2020-03-19 23:30:38 +0800

    1.递归求某数阶乘

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -24,16 +24,34 @@ public class Recursion {
         return results[n-1];
     }
 
+
+
+
     public static void main(String[] args) {
-        int n = 50;
-//        Recursion recursion = new Recursion();
-        Recursion recursion = new Recursion(n);
+        int n = 5;
+        Recursion recursion = new Recursion();
+        /*Recursion recursion = new Recursion(n);
         long startTime = System.nanoTime();
 //        int result = recursion.fibonacciFunctionV1(n);
         int result = recursion.fibonacciFunctionV2(n);
         long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
         System.out.println("斐波那契数列第" + n + "项值为： " + result);
-        System.out.println("fibonacciFunctionV1函数总用时:" + totalTime + "ns");
+        System.out.println("fibonacciFunctionV1函数总用时:" + totalTime + "ns");*/
+
+        int factorial = recursion.factorial(n);
+        System.out.println(n + "的阶乘为：" + factorial);
+
+
+
+    }
+
+    private int permutation(int[] array) {
+        return 0;
+    }
+
+    private int factorial(int n) {
+        if (n==1) return 1;
+        return factorial(n - 1) * n;
     }
 }
