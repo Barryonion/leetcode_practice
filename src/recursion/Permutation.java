424bf358ffd8eb014446ef2315a32543cbae1d87
zhang <zhangxucheng991@gmail.com>
2020-03-22 10:26:20 +0800

    1.重写求不同数的全排列

--- src/recursion/Permutation.java
+++ src/recursion/Permutation.java
@@ -4,34 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutation {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        // Arrays.sort(nums); // not necessary
-        backtrack(list, new ArrayList<>(), nums);
-        return list;
-    }
-
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-        if(tempList.size() == nums.length){
-            list.add(new ArrayList<>(tempList));
-        } else{
-            for(int i = 0; i < nums.length; i++){
-                if(tempList.contains(nums[i])) continue; // element already exists, skip
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums);
-                tempList.remove(tempList.size() - 1);
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        Permutation permutation = new Permutation();
+
         int[] nums = {1,2,3};
+        Permutation permutation = new Permutation();
         List<List<Integer>> listList = permutation.permute(nums);
         for (int i = 0; i < listList.size(); i++) {
             List<Integer> integers = listList.get(i);
             String s = integers.toString();
             System.out.println(s);
+        }
+
+
+    }
+
+    private List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> listList = new ArrayList<>();
+        backtrace(listList, new ArrayList<>(), nums);
+        return listList;
+    }
+
+    private void backtrace(List<List<Integer>> listList, ArrayList<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            listList.add(new ArrayList<>(tempList));//必须new 一个tempList对象保存当前排列，否则会被后续操作改变
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (tempList.contains(nums[i])) continue;
+                tempList.add(nums[i]);
+                backtrace(listList, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
         }
     }
 }
