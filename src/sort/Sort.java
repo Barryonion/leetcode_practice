029806a617a40a7f88fead7dc806e693500faf61
zhang <zhangxucheng991@gmail.com>
2020-03-22 17:19:02 +0800

    1.新增快速排序算法

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -3,11 +3,12 @@ package sort;
 public class Sort {
     public static void main(String[] args) {
         Sort sort = new Sort();
-        int[] nums = {23,45,1,45,75,37,38,86,234,33,75,8,45,657,5,6,97,45,8};
-//        int[] nums = {3,8,9,6,5,4,1,7};
+//        int[] nums = {23,45,1,45,75,37,38,86,234,33,75,8,45,657,5,6,97,45,8};
 //        int[] nums = {3,8,9,6};
+        int[] nums = {3,8,9,6,5,4,1,7};
         long startTime = System.nanoTime();
-        sort.mergeSort(nums, 0, nums.length - 1);
+//        sort.mergeSort(nums, 0, nums.length - 1);
+        sort.quickSort(nums,0,nums.length-1);
         long endTime = System.nanoTime();
         System.out.println("排序执行时长：" + (endTime - startTime));
         for (int i = 0; i < nums.length; i++) {
@@ -17,6 +18,38 @@ public class Sort {
             System.out.print(nums[i]+",");
         }
     }
+
+    private void quickSort(int[] nums, int lowIndex, int hignIndex) {
+        if (lowIndex >= hignIndex) {
+            return;
+        }
+        int patitionPoint = patitionPoint(nums, lowIndex, hignIndex);
+        quickSort(nums, lowIndex, patitionPoint - 1);
+        quickSort(nums, patitionPoint + 1, hignIndex);
+
+    }
+
+    private int patitionPoint(int[] nums, int lowIndex, int hignIndex) {
+        int pivot = nums[hignIndex];
+        int i = lowIndex;
+        for (int j = lowIndex; j < hignIndex; j++) {
+            if (nums[j] < pivot) {//交换
+                if (i == j) {//特殊情况
+                    i++;
+                } else {
+                    int tmp = nums[i];
+                    nums[i++] = nums[j];//注意i++
+                    nums[j] = tmp;
+                }
+            }
+        }
+        int tmp = nums[i];
+        nums[i] = pivot;
+        nums[hignIndex] = tmp;
+        return i;
+    }
+
+
 
     private void mergeSort(int[] nums, int lowIndex, int highIndex) {
         if (lowIndex == highIndex) {
