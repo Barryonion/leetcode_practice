800f04b5ea8d50af58ebc14dd23f634b53f92635
zhang <zhangxucheng991@gmail.com>
2020-11-14 10:07:12 +0800

    more concise solution

--- src/linkedlist/AddTwoNumber.java
+++ src/linkedlist/AddTwoNumber.java
@@ -5,43 +5,24 @@ public class AddTwoNumber {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode p = l1;
         ListNode q = l2;
-        int tens = 0;
-        ListNode head = new ListNode();
-        ListNode tail = head;
-        ListNode node;
-        int ds = 0;
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int carry = 0;
         while (p != null || q != null) {
-            if (p == null) {
-                ds = q.val + tens;
-            } else if (q == null) {
-                ds = p.val + tens;
-            } else {
-                ds = p.val + q.val + tens;
-            }
-            if (ds >= 10) {
-                int unit = ds - 10;
-                node = new ListNode(unit);
-                tens = 1;
-            } else {
-                node = new ListNode(ds);
-                tens = 0;
-            }
-            //使用尾插法创建单链表
-            tail.next = node;
-            tail = tail.next;
-            if (p!=null) {
-                p = p.next;
-            }
-            if (q!=null) {
-                q = q.next;
-            }
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            ListNode newNode = new ListNode(sum % 10);
+            current.next = newNode;
+            current = current.next;
+            if (p!=null) p = p.next;
+            if (q!=null) q = q.next;
         }
-        if (tens == 1) {//边界情况：l1、l2节点个数相同且相加大于10需进位
-            node = new ListNode(1);
-            tail.next = node;
-            tail = tail.next;
+        if (carry > 0) {
+            ListNode carryNode = new ListNode(carry);
+            current.next = carryNode;
         }
-        head = head.next;
-        return head;
+        return head.next;
     }
 }
