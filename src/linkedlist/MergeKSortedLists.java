342ac567bc59afd6ac9d7f6f1e59d5d3c714be9a
10156618 <zhangxucheng-hq@boe.com.cn>
2020-12-06 12:47:10 +0800

    add division solution to MergeKSortedLists

--- src/linkedlist/MergeKSortedLists.java
+++ src/linkedlist/MergeKSortedLists.java
@@ -1,12 +1,22 @@
 package linkedlist;
 
 public class MergeKSortedLists {
-    public ListNode mergeKLists(ListNode[] lists) {
+    public ListNode mergeKLists(ListNode[] lists) {//method1 : for loop using lists.length's merge operations
         ListNode arg = null;
         for (int i = 0; i < lists.length; i++) {
             arg = mergeTwoLists(arg, lists[i]);
         }
         return arg;
+    }
+    public ListNode merge(ListNode[] lists, int l, int r){//method2: division using log(lists.length) merge operations
+        if (l == r) {
+            return lists[l];
+        }
+        if (l > r) {
+            return null;
+        }
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
     }
 
     private ListNode mergeTwoLists(ListNode a, ListNode b) {
@@ -30,5 +40,21 @@ public class MergeKSortedLists {
         }
         tail.next = aPtr != null ? aPtr : bPtr;
         return head.next;
+    }
+
+    public static void main(String[] args) {
+        MergeKSortedLists mergeKSortedLists = new MergeKSortedLists();
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        ListNode head2 = new ListNode(2);
+        head2.next = new ListNode(3);
+        ListNode head3 = new ListNode(3);
+        head3.next = new ListNode(4);
+        ListNode[] list = new ListNode[3];
+        list[0] = head1;
+        list[1] = head2;
+        list[2] = head3;
+        ListNode merge = mergeKSortedLists.merge(list, 0, 2);
+        System.out.println(merge);
     }
 }
